46cf608a99b741bbac63008ea9f2cc0b6f9cd54f
GalmUI <ciphergalm0118@gmail.com>
2026-09-12 23:41:37 -0500

    Add R3 civic progression identifiers

---
+++ src/main/java/dev/conner/hometown/civic/ProgressionUnlock.java
@@ -0,0 +1,10 @@
+package dev.conner.hometown.civic;
+
+/** Durable R3 progression nodes. Gameplay for a node may arrive after the unlock itself. */
+public enum ProgressionUnlock {
+    TOWN_HALL,
+    NOTICE_BOARD,
+    CIVIC_PROJECTS,
+    STORAGE,
+    ANIMAL_FARMS
+}
